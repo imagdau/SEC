965a5ba9b13ab11550cb7e96b76ca6b96032b267
Ioan Magdau <i.b.magdau@gmail.com>
2020-07-16 10:52:17 -0700

    in genMDinput.m reorganized the input output paths. added signifincatly more MD trajectories in the examples, so the next clustering step is meaningful.

--- genMDinput.m
+++ genMDinput.m
@@ -3,35 +3,40 @@ crash_dumps_octave_core(0);
 warning off;
 addpath(genpath("octave-src"));
 
+######## input ########
 minDist = 2.3;
-N = 3;
+N = 100;
+inputPath = "example/input/";
+outputPath = "example/MD-sims/";
+#######################
 
 ### reading Lammps data file for the the equilibrated polymer
-if exist('example/input/seed.data.oct','file'),
-  load('example/input/seed.data.oct');
+if exist([outputPath,'seed.data.oct'],'file'),
+  load([outputPath,'seed.data.oct']);
 else,
   ### caching for large files
-  seed = readlmp('example/input/seed.data',true);
-  save('example/input/seed.data.oct','seed');
+  seed = readlmp([inputPath,'seed.data'],true);
+  save([outputPath,'seed.data.oct'],'seed');
 endif
 seed = unwrapPBClmp(seed);
 
 ### reading Lammps data file for the ion
-ion = readlmp('example/input/Li.data',true);
+ion = readlmp([inputPath,'Li.data'],true);
 
 ### adding ion to the equilibrium structure
 [lmp,lmpcol] = addSolvent(seed,ion,N,minDist);
 
 ### writing Lammps data and cif files with all ion positions for illustration
 [atoms,coords,latvec] = lmp2xyz(lmp);
-writelmp('example/MD-sims/illus.data',lmp);
-writecif('example/MD-sims/illus.cif',atoms,coords,latvec);
+writelmp([outputPath,'illus.data'],lmp);
+writecif([outputPath,'illus.cif'],atoms,coords,latvec);
 
 ### writing Lammps data files to be used as input for MD simulations
 for j=1:N,
   disp(j);
   fflush(stdout);
-  writelmp(sprintf('example/MD-sims/md-%04d.data',j),lmpcol{j});
+  fname = sprintf([outputPath,'md-%04d.data'],j);
+  writelmp(fname,lmpcol{j});
 endfor
 
 ### example Lammps command, pass correct variables to md.input
